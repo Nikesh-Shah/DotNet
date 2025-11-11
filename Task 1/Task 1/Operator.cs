7b70e2280120c2189a965c9881284dc1b683c9e9
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2025-11-11 08:27:36 +0545

    first task completed

---
+++ Task 1/Task 1/Operator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Operator
+{
+     public static int AddNum(int x, int y)
+    {
+        return x + y;
+
+    }
+    public static int SubtractNum(int x, int y)
+    {
+        return x - y;
+    }
+    public static int MultiplyNum(int x, int y)
+    {
+        return x * y;
+    }
+    public static int DivideNum(int x, int y)
+    {
+        if (y == 0)
+        {
+            throw new DivideByZeroException("Denominator cannot be zero.");
+        }
+        return x / y;
+    }
+
+    public static String  OddEvenFinder(int x)
+    {
+        return x % 2 == 0? "Even":"Odd";
+    }
+}
