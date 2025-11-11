5623c64c7343bc471a87ab220d9b2f134c0dd886
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2025-11-11 09:08:29 +0545

    task 3 completed

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age <= 13)
+{
+    Console.WriteLine("You are Child.");
+}
+else if (age >13 && age <= 19)
+{
+    Console.WriteLine("You are Teenager.");
+}
+else
+{
+       Console.WriteLine("You are Adult.");
+}
